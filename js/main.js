d8fb967e8a181d458a1a79ed94a0956c2427be91
Tornike Epitashvili <itorn9777@gmail.com>
2022-06-21 01:03:47 +0400

    added header & service, slider sections (imported swiper.js library)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// import and initialize swiper slider
+import "https://unpkg.com/swiper@8/swiper-bundle.min.js";
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.5,
+  spaceBetween: 30,
+  freeMode: true,
+});
 // open & close burger menu
 let open = false;
 window.onclick = (e) => {
@@ -9,14 +16,14 @@ window.onclick = (e) => {
   let closeTarget = e.target == closeBtn;
   let closeInnerTarget = e.target == burgerContent;
 
-  if (!open && openTarget) {
-    console.log("red");
-    burgerContent.style.right = "0px";
-    openBtn.style.display = "none";
-    closeBtn.style.display = "inline-block";
-    open = true;
+  if (!open) {
+    if (e.target === openBtn) {
+      burgerContent.style.right = "0px";
+      openBtn.style.display = "none";
+      closeBtn.style.display = "inline-block";
+      open = true;
+    }
   } else if ((open && closeTarget) || !closeInnerTarget) {
-    console.log("black");
     burgerContent.style.right = "-280px";
     closeBtn.style.display = "none";
     openBtn.style.display = "inline-block";
